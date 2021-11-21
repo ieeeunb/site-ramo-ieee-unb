7b3db4626922f41d517bdcfa786876138d194c66
felipe-costa-ft <felipecosta@ieee.org>
2021-11-21 11:12:15 -0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "Ramo IEEE UnB",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
